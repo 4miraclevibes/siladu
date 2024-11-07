23afb1e171578f92b0866f57146bfaeff5aad319
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-11-08 03:31:57 +0700

    cc

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\Role;
 use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
